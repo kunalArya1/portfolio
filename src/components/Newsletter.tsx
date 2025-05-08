8c9aa8b316b380b9e686189508c2e2095a4980d7
kunalArya1 <kunalkrraj220@gmail.com>
2025-05-08 22:03:00 +0530

    Add new images and enhance homepage layout with profile and case studies sections

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,25 +1,32 @@
 export default function NewLetter() {
   return (
-    <section className="w-full h-[50%] bg-[#292929] rounded-lg my-20 flex items-center justify-center text-center">
-      <div className="w-[40%]">
-        <h1 className=" font-bold text-3xl m-b-5">
+    <section className="w-full bg-[#3f3f3f] rounded-xl my-20 px-4 py-12 flex items-center justify-center text-center">
+      <div className="w-full max-w-2xl">
+        <h1 className="text-white font-bold text-2xl md:text-4xl mb-4">
           Subscribe to My Newsletter
         </h1>
-        <h4>
+        <p className="text-gray-300 mb-6 text-sm md:text-base">
           Subscribe to my email newsletter to get the latest posts delivered
-          right to your email. Pure inspiration, zero spam.
-        </h4>
-        <div className=" p-1 rounded-3xl flex justify-between items-center bg-[#767676]">
+          right to your email.
+          <br className="hidden sm:block" /> Pure inspiration, zero spam.
+        </p>
+
+        <div className="flex flex-row sm:flex-row items-center justify-center bg-[#595959] rounded-full p-1 mb-4">
           <input
-            type="text"
-            className=" outline-none mx-4"
-            placeholder="Enter Email"
+            type="email"
+            placeholder="Your E-mail"
+            className="flex-1 px-4 py-2 text-white bg-transparent placeholder-gray-300 focus:outline-none rounded-full w-full sm:w-auto"
           />
-          <button className=" p-1 bg-[#7728bc] rounded-3xl px-5 py-2">
-            Subsscribe
+          <button className=" sm:mt-0 sm:ml-2 bg-[#7e3ff2] hover:bg-[#692cce] text-white font-semibold text-sm px-6 py-2 rounded-full transition">
+            Subscribe <span className="ml-1">→</span>
           </button>
         </div>
-        <p>You agree to the Terms of Use and Privacy Policy</p>
+
+        <p className="text-gray-300 text-xs">
+          You agree to the{" "}
+          <span className="font-semibold text-white">Terms of Use</span> and{" "}
+          <span className="font-semibold text-white">Privacy Policy</span>
+        </p>
       </div>
     </section>
   );
